cc0c9ca3eb60bc538d8ae0e716669431adcd26a4
rodrigostuqui <rst.monzani@gmail.com>
2022-10-10 16:09:24 -0300

    feat: adding memory to project

--- memory.vhd
+++ memory.vhd
@@ -1,5 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity memory is
 generic (
     addr_width: natural := 16; -- Memory Address Width (in bits)
@@ -17,3 +19,21 @@ port (
     data_out : out std_logic_vector((data_width*4)-1 downto 0)
 );
 end entity;
+
+architecture behavior of memory is
+    subtype data_length is std_logic_vector(data_width-1 downto 0);
+    type mem is array (0 to 2**addr_width) of data_length;
+
+    signal memor : mem;
+begin    
+
+        memor(to_integer(unsigned(data_addr))) <= data_in 
+        when (falling_edge(clock) and data_read = '1' and data_write='0');
+
+        data_out <= (memor(to_integer(unsigned(data_addr)))
+        & memor(to_integer(unsigned(data_addr)) + 1)
+        & memor(to_integer(unsigned(data_addr)) + 2) 
+        & memor(to_integer(unsigned(data_addr)) + 3)) 
+        when (falling_edge(clock) and data_read = '0' and data_write = '1');
+
+end architecture behavior;
