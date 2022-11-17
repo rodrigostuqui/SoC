070873a6b52ef00d0371ab72167a5c7c20cf6505
rodrigostuqui <rst.monzani@gmail.com>
2022-11-16 22:29:16 -0300

    Fix: adding signals to mux

--- soc.vhd
+++ soc.vhd
@@ -16,7 +16,6 @@ end entity;
 architecture structural of soc is
     constant addr_width: natural := 16;
     constant data_width: natural := 8;
-    constant flag: boolean := true;
     signal halt: std_logic := '1';
     signal instruction_in: std_logic_vector((data_width*4)-1 downto 0) := (others => '0');
     signal instruction_addr: std_logic_vector(addr_width-1 downto 0) := (others => '0');
@@ -31,10 +30,11 @@ architecture structural of soc is
     signal codec_valid: std_logic := '0';
     signal codec_data_out : std_logic_vector(7 downto 0) := (others => '0');
     signal codec_data_in : std_logic_vector(7 downto 0) := (others => '0');
-    signal instruction_read: std_logic := '1';
+    signal instruction_read: std_logic := '0';
     signal instruction_write: std_logic := '0';
     signal instruction_out: std_logic_vector((data_width*2)-1 downto 0) := (others => '0');
-    signal endereco: std_logic_vector(15 downto 0) := (others => '0');
+    signal endereco_inst: std_logic_vector(15 downto 0) := (others => '0');
+    signal endereco1_inst: std_logic_vector(15 downto 0) := (others => '0');
 
 
     begin
@@ -44,41 +44,52 @@ architecture structural of soc is
     variable text_line: line;
     variable auxiliar: bit_vector(7 downto 0);
     begin
-        if started = '0' then
-            instruction_write <= '1';
+        if started = '0' and not endfile(arq) then
             halt <= '1';
-            if falling_edge(clock) and not endfile(arq) then
-                readline(arq,text_line);
-                read(text_line,auxiliar);
-                instruction_out(7 downto 0) <= std_logic_vector(unsigned(to_stdlogicvector(auxiliar)));
-                instruction_out(15 downto 8) <= "00000000";
-                instruction_addr <= endereco;
-                endereco <= std_logic_vector(unsigned(endereco) + 1);
-                wait for 5 ns;
-                report integer'image(to_integer(unsigned(endereco)));
+            instruction_read <= '0';
+            instruction_write <= '1';
+            readline(arq,text_line);
+            read(text_line,auxiliar);
+            wait for 5 ns;
+            instruction_out(15 downto 8) <= std_logic_vector(unsigned(to_stdlogicvector(auxiliar)));
+            instruction_out(7 downto 0) <= "00000000";
+            wait for 5 ns;
+            instruction_write <= '0';
+            if not endfile(arq) then
+                endereco_inst <= std_logic_vector(unsigned(endereco_inst) + 1);
             end if;
-        else
-            halt <= '0';
-            endereco <= instruction_addr;
+        elsif started = '1' and endfile(arq) then
+            if endereco_inst = endereco1_inst then
+                halt <= '1';
+            else 
+                halt <= '0';
+            end if;
+
+            instruction_read <= '1';
+            instruction_write <= '0';
+            wait for 5 ns;
         end if;
-        wait on clock;
+        wait until falling_edge(clock);
     end process;
     
+    mux_imem: entity work.mux2x1(behavioral)
+        generic map(addr_width)
+        port map(input0 => endereco_inst, input1 => endereco1_inst, sel => started, output => instruction_addr);
     
-        cpu1: entity work.cpu(behavior)
-        generic map(addr_width, data_width)
-        port map(halt => halt, instruction_in => instruction_in((data_width*4)-1 downto (data_width*4)-8), instruction_addr => instruction_addr, 
-            mem_data_read => mem_data_read, mem_data_write => mem_data_write, mem_data_addr => mem_data_addr,
-            mem_data_in => mem_data_in, mem_data_out => mem_data_out, codec_interrupt => codec_interrupt, codec_read => codec_read,
-            codec_write => codec_write, codec_valid => codec_valid, codec_data_out => codec_data_out, codec_data_in => codec_data_in, clock => clock);
+    cpu1: entity work.cpu(behavior)
+    generic map(addr_width, data_width)
+    port map(halt => halt, instruction_in => instruction_in((data_width*4)-1 downto (data_width*4)-8), instruction_addr => endereco1_inst, 
+        mem_data_read => mem_data_read, mem_data_write => mem_data_write, mem_data_addr => mem_data_addr,
+        mem_data_in => mem_data_in, mem_data_out => mem_data_out, codec_interrupt => codec_interrupt, codec_read => codec_read,
+        codec_write => codec_write, codec_valid => codec_valid, codec_data_out => codec_data_out, codec_data_in => codec_data_in, clock => clock);
 
     dmem: entity work.memory(behavior)
-    generic map(addr_width, data_width)
-    port map(clock, mem_data_read, mem_data_write, mem_data_addr, mem_data_in, mem_data_out);
-
+        generic map(addr_width, data_width)
+        port map(clock => clock, data_read => mem_data_read, data_write => mem_data_write,data_addr => mem_data_addr, data_in => mem_data_in, data_out => mem_data_out);
+        
     imem: entity work.memory(behavior)
         generic map(addr_width, data_width)
-        port map(clock, instruction_read, instruction_write, endereco, instruction_out, instruction_in);
+        port map(clock => clock, data_read => instruction_read, data_write => instruction_write,data_addr => instruction_addr, data_in => instruction_out, data_out => instruction_in);
 
     codec: entity work.codec(behavior)
         port map(codec_interrupt, codec_read, codec_write, codec_valid, codec_data_in, codec_data_out);
